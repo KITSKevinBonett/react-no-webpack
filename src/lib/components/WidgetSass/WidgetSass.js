9b481d8d6c50e44edb11da29929d61ae6b659a50
basher <basher@madasafish.com>
2018-03-02 17:23:30 +0000

    Add all files

---
+++ src/lib/components/WidgetSass/WidgetSass.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// import classNames from 'classnames';
+// import styles from './WidgetSass.scss';
+
+export default class WidgetSass extends Component {
+    render() {
+        return (
+            <p className="widget">Widget + Sass</p>
+        );
+    }
+}
