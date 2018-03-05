4f9f34056efca8415c7a7c0e8096cb9ee62d9471
KITSKevinBonett <kevin.bonett@kingfisher.com>
2018-03-05 11:20:01 +0000

    tried adding Sass support as per plugin docs

--- src/lib/components/WidgetSass/WidgetSass.js
+++ src/lib/components/WidgetSass/WidgetSass.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 // import classNames from 'classnames';
-// import styles from './WidgetSass.scss';
+import styles from './WidgetSass.scss';
 
 export default class WidgetSass extends Component {
     render() {
         return (
-            <p className="widget">Widget + Sass</p>
+            <p>Widget + Sass</p>
         );
     }
 }
